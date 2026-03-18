aae9c5fdd684808cf466ecfb496ad6a378175595
Tanush Angrish <Tanush.Angrish@adventz.com>
2026-03-18 17:40:55 +0530

    change in vite.config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,32 +1,32 @@
-// import { defineConfig, loadEnv } from 'vite'
-// import react from '@vitejs/plugin-react'
-// import tailwindcss from '@tailwindcss/vite'
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 
 
 
-// // https://vitejs.dev/config/
-// export default defineConfig(({ mode }) => {
-//   // Load env file based on `mode`. The third param '' loads all variables.
-//   const env = loadEnv(mode, process.cwd(), '');
-//   const apiTarget = env.VITE_API_URL || 'http://localhost:5000';
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  // Load env file based on `mode`. The third param '' loads all variables.
+  const env = loadEnv(mode, process.cwd(), '');
+  const apiTarget = env.VITE_API_URL || 'http://localhost:5000';
 
-//   return {
-//     plugins: [
-//       tailwindcss(),
-//       react()
-//     ],
-//     server: {
-//       proxy: {
-//         '/api': {
-//           target: apiTarget,
-//           changeOrigin: true,
-//         },
-//         '/uploads': {
-//           target: apiTarget,
-//           changeOrigin: true,
-//         }
-//       }
-//     }
-//   }
-// })
+  return {
+    plugins: [
+      tailwindcss(),
+      react()
+    ],
+    // server: {
+    //   proxy: {
+    //     '/api': {
+    //       target: apiTarget,
+    //       changeOrigin: true,
+    //     },
+    //     '/uploads': {
+    //       target: apiTarget,
+    //       changeOrigin: true,
+    //     }
+    //   }
+    // }
+  }
+})
